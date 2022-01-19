d024c90cb546a04ab2e59e9328fcbda494d3141a
GauravThakkar03 <73106285+GauravThakkar03@users.noreply.github.com>
2022-01-19 13:44:59 +0530

    update

--- src/main/java/io/threatrix/threatmatrix/examples/javascript/jquery/selector-v1.2.3-snippet-modified.js
+++ src/main/java/io/threatrix/threatmatrix/examples/javascript/jquery/selector-v1.2.3-snippet-modified.js
@@ -1,3 +1,34 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://jquery.com/
+*    Release: https://github.com/jquery/jquery/releases/tag/1.2.6
+*    Source File: selector.js
+*    
+*    Copyrights:
+*      copyright (c) 2007 john resig (jquery.com
+*      copyright (c) 2007 john resig, http://jquery.com
+*      copyright (c) 1989, 1991 free software foundation, inc
+*    
+*    Licenses:
+*      Lesser General Public License For Linguistic Resources
+*      SPDXId: LGPLLR
+*    
+*      GNU General Public License v2.0 only
+*      SPDXId: GPL-2.0-only
+*    
+*      OWTChart License
+*      SPDXId: xa0
+*    
+*      Verbatim Copies Permission
+*      SPDXId: xa0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 else {
 				re = /^([>+~])\s*(\w*)/i;
 
